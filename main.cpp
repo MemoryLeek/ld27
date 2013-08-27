368aad2fac25c81e4157ea4b0878976e67d5ce78
Znurre <znurree@gmail.com>
2013-08-27 10:28:58 +0200

    Removed custom QSurfaceFormat

--- main.cpp
+++ main.cpp
@@ -16,11 +16,11 @@ int main(int argc, char **argv)
 	QQmlComponent component(&engine, url);
 	QObject *object = component.create();
 
-	QSurfaceFormat format;
-	format.setSwapBehavior(QSurfaceFormat::SingleBuffer);
+//	QSurfaceFormat format;
+//	format.setSwapBehavior(QSurfaceFormat::SingleBuffer);
 
 	Window *window = (Window *)object;
-	window->setFormat(format);
+//	window->setFormat(format);
 	window->setWidth(800);
 	window->setHeight(600);
 	window->initialize();
